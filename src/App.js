0ab4517ae51de424a83c052f641f32438ce220cb
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2023-12-26 01:30:35 +0500

    Project Completed with Responsiveness

--- src/App.js
+++ src/App.js
@@ -53,26 +53,26 @@ function App() {
   }
 
   return (
-    <div className="w-full h-[100vh] bg-[#190924] flex flex-col justify-center s_mobile:items-start laptop:items-center gap-[50px]">
-      <h1 className="text-6xl text-white">Password Generator &#128640; </h1>
-      <div className="laptop:w-[800px] laptop:max-w-[1000px] s_mobile:w-[80%] bg-[#fff] p-[20px] rounded-2xl">
+    <div className="w-full h-[100vh] bg-[#190924] flex flex-col tablet:justify-center items-center s_mobile:justify-start gap-[50px] s_mobile:pt-[40px]">
+      <h1 className="tablet:text-6xl s_mobile:text-2xl text-white">Password Generator &#128640; </h1>
+      <div className="laptop:w-[800px] laptop:max-w-[1000px] s_mobile:w-[92%] bg-[#fff] tablet:p-[20px] rounded-2xl s_mobile:py-[15px] s_mobile:px-[10px]">
 
         {/* Top Line */}
-        <div className="flex items-stretch w-[100%]">
+        <div className="flex items-stretch w-[100%]  tablet:p-[15px]  s_mobile:justify-center s_mobile:w-full">
 
           {/* Generated Password */}
           <input 
             type={passwordType}
             value={password}
             ref={passwordRef}
-            className="py-[10px] px-[25px] w-[600px] rounded-tl-[10px] rounded-bl-[10px] outline-none border-slate-600 border-l border-t border-b"
+            className="tablet:py-[10px] s_mobile:py-[8px] tablet:px-[25px] s_mobile:px-[10px] tablet:w-[600px] s_mobile:w-[70%] tablet:rounded-l-[10px] s_mobile:rounded-l-[10px] s_mobile:text-[13px] m_mobile:text-md l_mobile:text-lg  outline-none border-slate-600 border-l border-t border-b"
             placeholder='Your Generated Password is Here' 
             readOnly
           />
 
           {/* Hide/Show Password */}
           <div 
-            className=" bg-white flex justify-center items-center p-[10px] text-3xl rounded-tr-[10px] rounded-br-[10px] cursor-pointer border-slate-600 border-r border-t border-b"
+            className=" bg-white flex justify-center items-center tablet:p-[10px] s_mobile:px-[5px] tablet:text-3xl s_mobile:text-xl tablet:rounded-r-[10px] s_mobile:rounded-r-[10px] cursor-pointer border-slate-600 border-r border-t border-b"
             onClick={changeVisibility}
             >
             {
@@ -82,7 +82,7 @@ function App() {
 
           {/* Copy Password */}
           <span 
-            className= " bg-[#00cf67] relative right-[-10px] border px-[15px] py-[10px] flex justify-center items-center  gap-[5px] cursor-pointer rounded-lg active:scale-[0.95]  text-lg"
+            className= " bg-[#00cf67] relative tablet:right-[-10px] m_mobile:right-[-3px] tablet:border tablet:px-[15px] s_mobile:px-[5px] s_mobile:rounded-lg tablet:py-[10px] flex justify-center items-center  gap-[5px] cursor-pointer tablet:rounded-lg active:scale-[0.95]  tablet:text-lg s_mobile:text-sm m_mobile:text-md l_mobile:text-lg"
             style={{ userSelect: 'none' , backgroundColor : isCopied === 'Copy' ? '#F6AE2D' : '#00cf67'}}
             onClick={copyToClipboard}
             >
@@ -91,9 +91,9 @@ function App() {
         </div>
 
         {/* Middle Area */}
-        <div className="text-[#10192D] text-xl flex justify-around mt-[20px]">
+        <div className="text-[#10192D] s_mobile:text-sm m_mobile:text-md l_mobile:text-lg tablet:text-xl flex justify-around mt-[20px] tablet:flex-row s_mobile:flex-col">
 
-          <div className="">
+          <div className="tablet:block s_mobile:flex ">
             <label className="">Length </label>
             <input 
               type="range"
@@ -131,6 +131,10 @@ function App() {
         {/* Button */}
       </div>
       <button className="py-[10px] px-[25px] text-white bg-[#009eac] rounded-2xl active:scale-[0.95]" onClick={generatePassword} >Generate Password</button>
+
+
+      {/* Made By Ahmed Ayaz */}
+      <span className="text-white laptop:text-xl absolute bottom-[20px] right-[20px]">Made By <a className="underline text-[#1a79a5]" href="https://github.com/aahmedayaz"> Ahmed Ayaz 🚀</a></span>
     </div>
   );
 }
